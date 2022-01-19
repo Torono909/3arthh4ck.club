f71508edd43c2d36ffb11af8657f89d304a4ec20
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-01-19 10:28:00 +0530

    Update Earthhack.java

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -12,8 +12,8 @@ import org.lwjgl.opengl.Display;
 
 public class Earthhack
         implements Globals {
-    private static final Logger LOGGER = LogManager.getLogger((String)"3arthh4ck");
-    public static final String NAME = "3arthh4ck.club";
+    private static final Logger LOGGER = LogManager.getLogger((String)"Toxonic");
+    public static final String NAME = "Toxonic";
     public static final String VERSION = "1.0.0";
 
     public static void preInit() {
@@ -21,11 +21,11 @@ public class Earthhack
     }
 
     public static void init() {
-        LOGGER.info("\n\nInitializing 3arthh4ck.club.");
-        Display.setTitle((String)"3arthh4ck.club - 1.0.0");
+        LOGGER.info("\n\nInitializing Toxonic.");
+        Display.setTitle((String)"Toxonic - 1.0.0");
         DiscordPresence.start();
         Managers.load();
-        LOGGER.info("\n3arthh4ck.club initialized.\n");
+        LOGGER.info("\nToxonic initialized.\n");
     }
 
     public static void postInit() {
